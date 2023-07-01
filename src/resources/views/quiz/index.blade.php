092c0ba560188989aa87d767eb773ab67be23ccc
Gaku1031 <gaku.1031.basket@gmail.com>
2023-07-02 01:39:37 +0900

    feat: :art: 更新完了メッセージを閉じられるように変更

--- src/resources/views/quiz/index.blade.php
+++ src/resources/views/quiz/index.blade.php
@@ -10,10 +10,21 @@
 <body class="bg-gray-100">
     <!-- フラッシュメッセージがある場合に表示 -->
     @if (session('message'))
-        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-            <span class="block sm:inline">{{ session('message') }}</span>
-        </div>
+        <div id="messageBox" class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+        <span class="block sm:inline">{{ session('message') }}</span>
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg id="closeButton" class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        </span>
+    </div>
     @endif
+
+    <script>
+        window.onload = function () {
+            document.getElementById('closeButton').addEventListener('click', function () {
+                document.getElementById('messageBox').style.display = 'none';
+            });
+        };
+    </script>
 
     <h1 class="font-bold text-3xl text-center mt-5">クイズ一覧</h1>
     <div class="container mx-auto py-10">
